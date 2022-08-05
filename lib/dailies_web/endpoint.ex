3878734f3c7f9d01aa8a1e1a3ce888f660f1575d
Yan Bussieres <yan.buss22@gmail.com>
2022-08-05 08:27:19 -0400

    fix plug name

--- lib/dailies_web/endpoint.ex
+++ lib/dailies_web/endpoint.ex
@@ -10,43 +10,40 @@ defmodule DailiesWeb.Endpoint do
     signing_salt: "27VIP0e2"
   ]
 
-  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug(Plug.Static,
+  plug Plug.Static,
     at: "/",
     from: :dailies,
     gzip: false,
     only: ~w(assets fonts images favicon.ico robots.txt)
-  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug(Phoenix.CodeReloader)
-    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :dailies)
+    plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :dailies
   end
 
-  plug(Phoenix.LiveDashboard.RequestLogger,
+  plug Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
-  )
 
-  plug(Plug.RequestId)
-  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug(Plug.Parsers,
+  plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
-  )
 
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
-  plug(Plug.Session, @session_options)
-  plug(CORSPlug)
-  plug(DailiesWeb.Router)
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug CORSPlug
+  plug DailiesWeb.Router
 end
